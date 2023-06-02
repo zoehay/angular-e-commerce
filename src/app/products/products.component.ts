bca45dfc23fd24438a3219d67d52cd491afc38ee
Zoe Hay <zoemhay@gmail.com>
2023-06-02 16:14:50 -0700

    basic product tile style

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,17 +8,68 @@ import { Product } from '../product';
   standalone: true,
   imports: [CommonModule, ProductComponent],
   template: `
-    <section class="product-feed">
-      <app-product [product]="product"></app-product>
-    </section>
+    <div class="page-content">
+      <section class="product-feed">
+        <app-product
+          *ngFor="let product of productsList"
+          [product]="product"
+        ></app-product>
+      </section>
+    </div>
   `,
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
-  product: Product = {
-    id: 1,
-    name: 'banana',
-    description: 'squishy',
-    price: 2,
-  };
+  productsList: Product[] = [
+    {
+      id: 1,
+      name: 'Shovel',
+      description: "It's like a big spoon for dirt and stuff.",
+      price: 9.0,
+    },
+    {
+      id: 1,
+      name: 'Bug Net',
+      description: 'Catch those creepy crawlies.',
+      price: 11.0,
+    },
+    {
+      id: 1,
+      name: 'Fishing Rod',
+      description: 'A rod used for fishing and the like.',
+      price: 12.6,
+    },
+    {
+      id: 1,
+      name: 'BBQ',
+      description: 'Throw some stuff on this. It will then cook..',
+      price: 340.0,
+    },
+    {
+      id: 1,
+      name: 'Basic Axe',
+      description: 'Chop down trees and plants..',
+      price: 10.0,
+    },
+    { id: 1, name: 'Bananas', description: 'Squishy', price: 4.96 },
+    { id: 1, name: 'Bush Lime', description: 'Sour but yummy.', price: 1.8 },
+    {
+      id: 1,
+      name: 'Torch',
+      description: "This isn't a flashlight... its a torch!",
+      price: 60.0,
+    },
+    {
+      id: 1,
+      name: 'Wheelbarrow',
+      description: 'Fill it with dirt or other stuff.',
+      price: 300.0,
+    },
+    {
+      id: 1,
+      name: 'Table Saw',
+      description: 'Can cut wood into planks.',
+      price: 52.0,
+    },
+  ];
 }
