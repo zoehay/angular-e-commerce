33c13fa9c671988a106aa139cc9741cc6d926c1a
Zoe Hay <zoemhay@gmail.com>
2023-06-21 16:56:25 -0700

    homepage text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,10 @@ import { CommonModule } from '@angular/common';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>Welcome to the store! It will be the same but better!</p> `,
+  template: `
+    <h2>Welcome to the store!</h2>
+    <p>Now with Angular! You can make up a user to add products to your cart</p>
+  `,
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {}
