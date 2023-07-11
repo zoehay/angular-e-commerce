94218a43936eca5855af89fdeb18c1a62edd7329
Zoe Hay <zoemhay@gmail.com>
2023-07-11 15:25:41 -0700

    add login test

--- tests/page-objects/login-page.js
+++ tests/page-objects/login-page.js
@@ -1,9 +1,20 @@
+import { By, Builder, Browser } from "selenium-webdriver";
+
 export class LoginPage {
   constructor(driver) {
     this.driver = driver;
   }
 
   async goTo() {
-    await this.driver.get("http://localhost:3000/auth/login");
+    await this.driver.get("http://localhost:4200/auth/login");
+  }
+
+  async login() {
+    let emailInput = await this.driver.findElement(By.id("email"));
+    let passwordInput = await this.driver.findElement(By.id("password"));
+    let loginButton = await this.driver.findElement(By.id("submit"));
+    await emailInput.sendKeys("john@email");
+    await passwordInput.sendKeys("stuff");
+    await loginButton.click();
   }
 }
